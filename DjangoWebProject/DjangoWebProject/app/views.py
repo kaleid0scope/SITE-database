ab070674e7e98aff010ddefc5904c5777b3d6887
GONG <gongqixaing@uibesea.org>
2017-03-03 23:40:38 +0800

    这周最后一次提交了。

--- DjangoWebProject/DjangoWebProject/app/views.py
+++ DjangoWebProject/DjangoWebProject/app/views.py
@@ -40,7 +40,10 @@ def home(request):
         })
 
 def first(request):
-    return render(request,'first.html',{ 'title':'学生主页',})
+    return render(request,'first.html')
+
+def shit(request):
+    return render_with_type(request,'app/shit.html',{})
 
 def contact(request):
     return render_with_type(request,'app/contact.html', {'t': settings.STATIC_ROOT})
