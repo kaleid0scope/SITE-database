e5a21903eadcba2afcd6fe17c4d807115e414ab1
GONG <gongqixaing@uibesea.org>
2017-04-30 17:20:28 +0800

    数据库。

--- DjangoWebProject/DjangoWebProject/app/admin.py
+++ DjangoWebProject/DjangoWebProject/app/admin.py
@@ -3,7 +3,6 @@ from app.models import *
  
  
 admin.site.register(Students)
-admin.site.register(Authorizations)
 admin.site.register(ResearchProjectRank)
 admin.site.register(ResearchProject)
 admin.site.register(LectureRank)
